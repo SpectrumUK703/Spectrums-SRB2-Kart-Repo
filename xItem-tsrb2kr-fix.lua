ab74883e90f5ab2c41c9fed1667f828184c151a7
SpectrumUK703 <Jonathan@vegan4life.org.uk>
2023-05-04 18:49:45 +0100

    Update xItem-tsrb2kr-fix.lua

--- xItem-tsrb2kr-fix.lua
+++ xItem-tsrb2kr-fix.lua
@@ -1,9 +1,44 @@
-if not (xItemLib and tsrb2kr) then return end
+if not xItemLib then return end
 
-local xItem_FindHudFlags_Original = xItemLib.func.hudFindFlags
+local BASEVIDWIDTH  = 320
+local BASEVIDHEIGHT = 200
+
+local ITEM_X = 5
+local ITEM_Y = 5
+
+local ITEM1_X = -9
+local ITEM1_Y = -8
+
+local ITEM2_X = BASEVIDWIDTH-39
+local ITEM2_Y = -8
+local colormode = TC_RAINBOW
+local localcolor = SKINCOLOR_NONE
+local V_SNAPTOTOP = V_SNAPTOTOP
+local V_SNAPTOLEFT = V_SNAPTOLEFT
+local V_SNAPTORIGHT = V_SNAPTORIGHT
+local V_SPLITSCREEN = V_SPLITSCREEN
+local V_HUDTRANS = V_HUDTRANS
+
+local function xItem_FindHudFlags_Original(v, p, c)
+	if splitscreen < 2 then -- don't change shit for THIS splitscreen.
+		if c.pnum == 1 then
+			return ITEM_X, ITEM_Y, V_SNAPTOTOP|V_SNAPTOLEFT, false
+		else
+			return ITEM_X, ITEM_Y, V_SNAPTOLEFT|V_SPLITSCREEN, false
+		end
+	else -- now we're having a fun game.
+		if c.pnum == 1 or c.pnum == 3 then -- If we are P1 or P3...
+			return ITEM1_X, ITEM1_Y, (c.pnum == 3 and V_SPLITSCREEN or V_SNAPTOTOP)|V_SNAPTOLEFT, false	-- flip P3 to the bottom.	
+		else -- else, that means we're P2 or P4.
+			return ITEM2_X, ITEM2_Y, (c.pnum == 4 and V_SPLITSCREEN or V_SNAPTOTOP)|V_SNAPTORIGHT, true
+		end
+	end
+end
 
 local function xItem_FindHudFlags_Edit(v, p, c)
-	if not G_RaceGametype() then return xItem_FindHudFlags_Original(v, p, c) end
+	if not (tsrb2kr and G_RaceGametype()) 
+		return xItem_FindHudFlags_Original(v, p, c)
+	end
 	local x, y = 265, 5
 	if (splitscreen)
 		x, y = 270, -1
