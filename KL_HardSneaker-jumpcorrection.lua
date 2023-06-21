0b93f8497e766b135e96be5bf4547b11acc6b0ae
SpectrumUK703 <Jonathan@vegan4life.org.uk>
2023-06-21 19:59:58 +0100

    Not sure this works lol

---
+++ KL_HardSneaker-jumpcorrection.lua
@@ -0,0 +1,37 @@
+local k_sneakertimer = k_sneakertimer
+local k_speedboost = k_speedboost
+local FRACUNIT = FRACUNIT
+local TICRATE = TICRATE
+local MFE_SPRUNG = MFE_SPRUNG
+local cv_kartspeed = CV_FindVar("kartspeed")
+local hostmodload
+
+addHook("ThinkFrame", function()
+	if not (kmp and kmp_hardsneakers.value) then return end
+	cv_kartspeed = $ or CV_FindVar("kartspeed")
+	if cv_kartspeed.value ~= 2 then return end
+	for p in players.iterate
+		p.springjump = $ and $-1 or 0
+		local mo = p.mo
+		if p.spectator
+		or not (mo and mo.valid) then continue end
+		local sector = mo.subsector.sector
+		local special = GetSecSpecial(sector.special, 3)
+		if not P_IsObjectOnGround(mo)
+			if not p.jumped
+			and p.kartstuff[k_sneakertimer]
+			and not ((mo.eflags & MFE_SPRUNG) or special == 5 or p.springjump)
+				P_SetObjectMomZ(mo, -mo.momz*3/20, true)
+			end
+			p.jumped = true
+		else
+			p.jumped = false
+			if (mo.eflags & MFE_SPRUNG) or special == 5
+				p.springjump = 10
+			end
+		end
+	end
+	if hostmodload or not (server and HOSTMOD and leveltime > TICRATE) then return end
+	HM_Scoreboard_AddMod({disp = "JumpCorrection", var = "kmp_hardsneakers"})
+	hostmodload = true
+end)
---
+++ KL_karteliminatelast-tweaks.lua
@@ -0,0 +1,129 @@
+local k_position = k_position
+local PF_TIMEOVER = PF_TIMEOVER
+local TICRATE = TICRATE
+local raceexittime = 5*TICRATE + (2*TICRATE/3)
+local jointime = 6*TICRATE + (3*TICRATE/4) + (20*TICRATE)
+local playercount
+local ragespec
+local hostmodload = false
+local karteliminatelast = CV_FindVar("karteliminatelast")
+
+
+--Function copied from Friendmod and edited
+local function welcometofuckingdie(p)
+	p.pflags = $|PF_TIMEOVER
+	if p.mo and p.mo.valid
+		local mo = p.mo
+		--S_StopSound(mo)
+		P_DamageMobj(mo, nil, nil, 10000)
+		--S_StartSound(mo, sfx_kc3b)
+		--K_SpawnMineExplosion(mo, p.skincolor)
+		P_DamageMobj(mo, nil, nil, 10000)
+	end
+	S_ChangeMusic("krfail", true, p)
+end
+
+--Rest of this mod based on my MK-Style last-elimination mod
+local function theresultsarein(p)
+	if p.mo and p.mo.valid
+		local mo = p.mo
+		S_StartSound(mo, sfx_klose)
+		local fakeplayer = P_SpawnMobj(mo.x, mo.y, mo.z, MT_THOK)
+		fakeplayer.skin = mo.skin
+		fakeplayer.sprite = mo.sprite
+		fakeplayer.color = mo.color
+		fakeplayer.frame = mo.frame
+		fakeplayer.angle = mo.angle
+		fakeplayer.tics = 99999
+		P_DamageMobj(mo, nil, nil, 10000)
+	end
+	chatprintf(p, "\x82The results are in! The race is over.")
+	S_ChangeMusic("krlose", true, p)
+end
+
+local function ismapmariokart(map)
+	local mapinfo = mapheaderinfo[map]
+	local mapname = mapinfo.lvlttl
+	return mapname:find("MK")
+	or mapname:find("SNES")
+	or mapname:find("N64")
+	or mapname:find("GC")
+	or mapname:find("Wii") --Also covers WiiU
+	or mapname:find("GBA")
+	or mapname:find("DS") --Also covers 3DS
+end
+
+local function karteliminatelasttweaked(arg1)
+	if arg1.value and server
+		COM_BufInsertText(server, "karteliminatelast no")
+	end
+end
+
+local karteliminatelasttweaked = CV_RegisterVar({
+	name = "karteliminatelasttweaked",
+	defaultvalue = "On",
+	flags = CV_NETVAR|CV_CALL,
+	possiblevalue = CV_OnOff,
+	func = karteliminatelasttweaked
+})
+
+addHook("NetVars", function(net)
+	playercount = net($)
+	ragespec = net($)
+end)
+
+addHook("ThinkFrame", do
+	if not (server and karteliminatelasttweaked.value) then return end
+	karteliminatelast = $ or CV_FindVar("karteliminatelast")
+	if karteliminatelast.value
+		COM_BufInsertText(server, "karteliminatelast no")
+	end
+	if G_BattleGametype() then return end
+	if leveltime == jointime+1
+		playercount = 0
+		ragespec = false
+		for p in players.iterate
+			if p.spectator then continue end
+			playercount = $+1
+		end
+	end
+	if not ragespec and leveltime > jointime+1
+		local currentplayercount = 0
+		local finishedcount = 0
+		local position = 0
+		local lastplayer = nil
+		for p in players.iterate
+			if p.spectator then continue end
+			if (p.pflags & PF_TIMEOVER)
+				p.lives = 0
+				ragespec = true
+				continue
+			end
+			currentplayercount = $+1
+			if p.exiting
+				finishedcount = $+1
+			elseif p.kartstuff[k_position] > position
+				position = p.kartstuff[k_position]
+				lastplayer = p
+			end
+		end
+		if currentplayercount ~= playercount
+			ragespec = true
+			return
+		end
+		if playercount >= 2 and finishedcount == playercount-1
+		and lastplayer and lastplayer.valid and not lastplayer.spectator
+			ragespec = true --They didn't ragespec, but after this part, the code shouldn't need to run
+			if ismapmariokart(gamemap)
+				theresultsarein(lastplayer)
+			else
+				welcometofuckingdie(lastplayer)
+			end
+			lastplayer.pflags = $|PF_TIMEOVER
+			lastplayer.lives = 0
+		end
+	end
+	if hostmodload or not (server and HOSTMOD and HM_Scoreboard_AddMod and leveltime > TICRATE) then return end
+	HM_Scoreboard_AddMod({disp = "LastTweaks", var = "karteliminatelasttweaked"})
+	hostmodload = true
+end)
