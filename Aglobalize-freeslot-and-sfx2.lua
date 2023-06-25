2ec052c7142071f9e6ae1aae926101b866eb3c82
SpectrumUK703 <Jonathan@vegan4life.org.uk>
2023-06-25 01:55:30 +0100

    Create Aglobalize-freeslot-and-sfx2.lua

---
+++ Aglobalize-freeslot-and-sfx2.lua
@@ -0,0 +1,42 @@
+local g = rawset
+
+for i=1, #sfxinfo-1
+	local v = sfxinfo[i]
+	g(_G, "sfx_"..v.name, i)
+end
+
+local function table_pack(...)
+    return {n=select("#", ...), ...}
+end
+
+local fs = freeslot
+rawset(_G, "freeslot", function(...)
+
+	local args = {...}
+
+	-- call original freeslot function
+	local ints = table_pack(fs(unpack(args)))
+
+	-- globalize the results.
+	for i = 1, #args do
+
+		-- spr2 does some weird things here so let's leave it out for now.
+		--Either I've fixed whatever that was, or I haven't noticed it yet.
+		/*if string.upper(args[i]:sub(1, 5)) == "SPR2_"
+			continue
+		end*/
+
+		if string.upper(args[i]:sub(1, 4)) == "SFX_"
+			args[i] = string.lower(args[i])
+		else
+			args[i] = string.upper(args[i])
+		end
+
+		g(_G, args[i], nil) --so this fixes SRB2P's Globalize
+		--print("Globalize... "..tostring(args[i]).."->"..tostring(_G[args[i]]))
+
+		g(_G, args[i], _G[args[i]])
+	end
+	
+	return unpack(ints)
+end)
