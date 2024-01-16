1b54c3f4d64dde3098422969d91620016360a35a
SpectrumUK703 <Jonathan@vegan4life.org.uk>
2024-01-16 20:27:00 +0000

    Update KL_Boost-jumpcorrection-v4.lua

--- KL_Boost-jumpcorrection-v4.lua
+++ KL_Boost-jumpcorrection-v4.lua
@@ -21,9 +21,15 @@ local boosttable = {
 	{ 17294+768, 32000, 41000}
 }
 
+local sneakerpower
+local newmomz
+local mo
+local sector
+local special
+
 addHook("ThinkFrame", function()
 	if not cv_jumpcorrection.value then return end
-	local sneakerpower = sneakerpowertable[gamespeed+1]
+	sneakerpower = sneakerpowertable[gamespeed+1]
 	for p in players.iterate
 		if p.SPSstackedpanels
 			sneakerpower = boosttable[gamespeed+1][p.SPSstackedpanels]
@@ -31,18 +37,22 @@ addHook("ThinkFrame", function()
 			sneakerpower = sneakerpowertable[gamespeed+1]
 		end
 		p.springjump = $ and $-1 or 0
-		local mo = p.mo
+		mo = p.mo
 		if p.spectator
 		or not (mo and mo.valid) then continue end
-		local sector = mo.subsector.sector
-		local special = GetSecSpecial(sector.special, 3)
+		sector = mo.subsector.sector
+		special = GetSecSpecial(sector.special, 3)
 		if not P_IsObjectOnGround(mo)
 			if not p.jumped
-			and p.kartstuff[k_speedboost] > sneakerpower
+			and p.kartstuff[k_speedboost] > sneakerpower and mo.momz > 0
 			and not ((mo.eflags & MFE_SPRUNG) or special == 5 or p.springjump)
 				-- In the case of hardsneaker/firmsneaker, this is (50%-27.5%/150%)
-				--print(-mo.momz*((p.kartstuff[k_speedboost]-sneakerpower)/16)/((FRACUNIT+p.kartstuff[k_speedboost])/16))
-				P_SetObjectMomZ(mo, -mo.momz*((p.kartstuff[k_speedboost]-sneakerpower)/16)/((FRACUNIT+p.kartstuff[k_speedboost])/16), true)
+				newmomz = ((-mo.momz/256-1)*(1+p.kartstuff[k_speedboost]/256-sneakerpower/256)/(FRACUNIT/256+p.kartstuff[k_speedboost]/256))*256
+				--print(mo.momz)
+				--print(p.kartstuff[k_speedboost])
+				--print(sneakerpower)
+				--print(newmomz)
+				P_SetObjectMomZ(mo, newmomz, true)
 				--print("Jump corrected")
 			end
 			p.jumped = true
