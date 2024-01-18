23a5b2d2ed0c7ce0ac164cdb14713f3f170e8be9
SpectrumUK703 <Jonathan@vegan4life.org.uk>
2024-01-18 20:21:58 +0000

    Create hijacking-lmao.lua

---
+++ hijacking-lmao.lua
@@ -0,0 +1,15 @@
+local CV_RegisterVar_Vanilla = CV_RegisterVar
+
+-- hijacking lmao
+rawset(_G, "CV_RegisterVar", function(table)
+	if table and table[1] == "tumbleignoreiframes"
+		return CV_RegisterVar_Vanilla({"_"..table[1], table[2], table[3], table[4], table[5]})
+	end
+	return CV_RegisterVar_Vanilla(table)
+end)
+
+local FixedDiv_vanilla = FixedDiv
+
+rawset(_G, "FixedDiv", function(x, y)
+	return FixedDiv_vanilla(x, y or 1)
+end)
