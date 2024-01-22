572f9825797accecc8b193e2ca6d8914866ade8d
SpectrumUK703 <Jonathan@vegan4life.org.uk>
2024-01-22 19:34:46 +0000

    Update KL_Boost-jumpcorrection-v6.lua

--- KL_Boost-jumpcorrection-v6.lua
+++ KL_Boost-jumpcorrection-v6.lua
@@ -13,8 +13,6 @@ local cv_jumpcorrection = CV_RegisterVar({
     flags = CV_NETVAR,
 })
 
-local sneakerpowertable = {53740+768, 32768, 17294+768}
-
 -- for stackable panels
 local boosttable = {
 	{ 53740+768, 75000, 87500},
@@ -22,33 +20,30 @@ local boosttable = {
 	{ 17294+768, 32000, 41000}
 }
 
-local sneakerpower
-local newmomz
-local mo
-local sector
-local special
-local totalboost
-
 addHook("ThinkFrame", function()
 	if not cv_jumpcorrection.value then return end
-	sneakerpower = sneakerpowertable[gamespeed+1]
+	local sneakerpower
+	local newmomz
+	local mo
+	local sector
+	local special
+	local totalboost
+	local table
 	for p in players.iterate
-		if p.SPSstackedpanels
-			sneakerpower = boosttable[gamespeed+1][p.SPSstackedpanels]
-		else
-			sneakerpower = sneakerpowertable[gamespeed+1]
-		end
-		p.springjump = $ and $-1 or 0
+		p.jumpcorrectiontable = $ or {}
+		table = p.jumpcorrectiontable
+		sneakerpower = boosttable[gamespeed+1][p.SPSstackedpanels or 1]
+		table.springjump = $ and $-1 or 0
 		mo = p.mo
 		if p.spectator
 		or not (mo and mo.valid) then continue end
-		sector = mo.subsector.sector
+		sector = P_ThingOnSpecial3DFloor(mo) or mo.subsector.sector
 		special = GetSecSpecial(sector.special, 3)
 		totalboost = p.kartstuff[k_speedboost]+p.kartstuff[k_boostpower]-FRACUNIT
 		if not P_IsObjectOnGround(mo)
-			if not p.jumped
+			if not table.jumped
 			and totalboost > sneakerpower and mo.momz > 0
-			and not ((mo.eflags & MFE_SPRUNG) or special == 5 or p.springjump)
+			and not ((mo.eflags & MFE_SPRUNG) or special == 5 or table.springjump)
 				-- In the case of hardsneaker/firmsneaker, this is multiplying by (50%-27.5%/150%)
 				newmomz = FixedDiv(FixedMul(-mo.momz, totalboost-sneakerpower), FRACUNIT+totalboost or 1)
 				--print(mo.momz)
@@ -60,11 +55,11 @@ addHook("ThinkFrame", function()
 				P_SetObjectMomZ(mo, newmomz, true)
 				--print("Jump corrected")
 			end
-			p.jumped = true
+			table.jumped = true
 		else
-			p.jumped = false
+			table.jumped = false
 			if (mo.eflags & MFE_SPRUNG) or special == 5
-				p.springjump = 10
+				table.springjump = 10
 			end
 		end
 	end
