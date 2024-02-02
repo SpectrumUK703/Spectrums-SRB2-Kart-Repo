208e08689e8ef6adffaef44ed4daeba237f63946
SpectrumUK703 <Jonathan@vegan4life.org.uk>
2024-02-02 01:43:21 +0000

    Update maybe-reduce-lag.lua

--- maybe-reduce-lag.lua
+++ maybe-reduce-lag.lua
@@ -3,9 +3,9 @@ local MF_SCENERY = MF_SCENERY
 
 rawset(_G, "addHook", function(hook, fn, extra)
 	if hook == "MobjThinker" and extra == nil
-		for i=0, #mobjinfo
-			if mobjinfo[i] and not (mobjinfo[i].flags & MF_SCENERY)
-				addHook_vanilla(hook, fn, i)
+		for i=1, #mobjinfo
+			if mobjinfo[i-1] and not (mobjinfo[i-1].flags & MF_SCENERY)
+				addHook_vanilla(hook, fn, i-1)
 			end
 		end
 	else
