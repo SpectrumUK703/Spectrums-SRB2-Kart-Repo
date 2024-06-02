c7c2a5f362a04d32a23b7a3fbb97ef39e264bfee
SpectrumUK703 <34438887+SpectrumUK703@users.noreply.github.com>
2024-06-02 04:22:29 +0100

    for an exploit

---
+++ unrestrictskins-edited.lua
@@ -0,0 +1,41 @@
+-- restrictskinchange? cheat? on by default?
+-- nuh uh
+
+-- Let players switch characters during position (this is actually to prevent a specific exploit)
+local cv_unrestrictskinsposition = CV_RegisterVar({
+	name = "unrestrictskinsposition",
+	defaultvalue = "On",
+	possiblevalue = CV_OnOff,
+	flags = CV_NETVAR,
+	description = "Allow changing skins during position, and the first 20 seconds of the race.",
+})
+
+local cv_unrestrictskins = CV_RegisterVar({
+	name = "unrestrictskins",
+	defaultvalue = "Off",
+	possiblevalue = CV_OnOff,
+	flags = CV_NETVAR,
+	description = "Allow changing skins during gameplay.",
+})
+
+-- we have to not confuse this with an actual team change
+local skins = {}
+addHook("PostThinkFrame", function()
+	for p in players.iterate do
+		skins[#p] = p.skin
+	end
+end)
+
+-- this is crossing tic boundaries, so we have to sync it
+addHook("NetVars", function(sync)
+	skins = sync($)
+end)
+
+-- hope this works... o_o
+addHook("TeamSwitch", function(p, newteam, fromspectators, tryingautobalance, tryingscramble)
+	if (cv_unrestrictskins.value or (cv_unrestrictskinsposition.value and (leveltime <= starttime + 20*TICRATE))) and newteam == 0 and not (fromspectators or tryingautobalance or tryingscramble) then
+		if skins[#p] ~= p.skin then
+			return false
+		end
+	end
+end)
