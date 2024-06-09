a4ff62b1b6a6faf5d49bf9facac55afe813ce94c
SpectrumUK703 <34438887+SpectrumUK703@users.noreply.github.com>
2024-06-09 22:16:11 +0100

    Create DL_SuperstarxalienColors.lua

---
+++ DL_SuperstarxalienColors.lua
@@ -0,0 +1,46 @@
+freeslot(
+"SKINCOLOR_CHESTNUT",
+"SKINCOLOR_BATTERY",
+"SKINCOLOR_HALLOWEEN",
+"SKINCOLOR_SUNSET"
+)
+
+skincolors[SKINCOLOR_CHESTNUT] = {
+    name = "Chestnut",
+    ramp = {0,51,222,66,67,68,69,63,63,45,46,47,47,29,30,31
+},
+    invcolor = SKINCOLOR_PLATINUM,
+    invshade = 6,
+    chatcolor = V_GOLDMAP,
+    accessible = true
+}	
+
+skincolors[SKINCOLOR_BATTERY] = {
+    name = "Battery",
+    ramp = {80,81,83,72,73,75,92,21,23,24,25,26,27,28,29,31
+},
+    invcolor = SKINCOLOR_PIGEON,
+    invshade = 5,
+    chatcolor = V_YELLOWMAP,
+    accessible = true
+}	
+
+skincolors[SKINCOLOR_HALLOWEEN] = {
+    name = "Halloween",
+    ramp = {0,83,64,52,53,55,69,27,28,28,28,29,29,30,30,31
+},
+    invcolor = SKINCOLOR_PLATINUM,
+    invshade = 10,
+    chatcolor = V_ORANGEMAP,
+    accessible = true
+}
+
+skincolors[SKINCOLOR_SUNSET] = {
+    name = "Sunset",
+    ramp = {72,72,74,52,53,54,213,184,184,184,185,185,186,186,187,187
+},
+    invcolor = SKINCOLOR_PLAGUE,
+    invshade = 5,
+    chatcolor = V_ORANGEMAP,
+    accessible = true
+}
