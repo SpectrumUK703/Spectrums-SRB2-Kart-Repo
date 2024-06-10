31b9fd9dbcc59847e5e04996cb29c526c5438dcd
SpectrumUK703 <34438887+SpectrumUK703@users.noreply.github.com>
2024-06-10 01:06:54 +0100

    I may be stupid

--- Hahaha-im-gonna-hijack-addHook-again.lua
+++ Hahaha-im-gonna-hijack-addHook-again.lua
@@ -1,15 +1,15 @@
 local addHook_vanilla = addHook
 local hijacked
 
-rawset(_G, "addHook", function(hook, function, motype)
+rawset(_G, "addHook", function(hook, func, motype)
 	if hijacked then
-		return addHook_vanilla(hook, function, motype)
+		return addHook_vanilla(hook, func, motype)
 	end
 	if hook == "MobjDamage" and motype == nil	-- Fairly sure the function should only be running for MT_PLAYER
 		hijacked = true
 		print("all objects MobjDamage hook changed to only run for MT_PLAYER")
-		return addHook_vanilla(hook, function, MT_PLAYER)
+		return addHook_vanilla(hook, func, MT_PLAYER)
 	else
-		return addHook_vanilla(hook, function, motype)
+		return addHook_vanilla(hook, func, motype)
 	end
 end)
