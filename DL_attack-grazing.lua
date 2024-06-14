b4b280923d1719aa95b44632554c878978496247
SpectrumUK703 <34438887+SpectrumUK703@users.noreply.github.com>
2024-06-14 01:18:46 +0100

    more changes

--- DL_attack-grazing.lua
+++ DL_attack-grazing.lua
@@ -3,7 +3,10 @@ local MF2_ALREADYHIT = MF2_ALREADYHIT
 local KITEM_FLAMESHIELD = KITEM_FLAMESHIELD
 local KSHIELD_TOP = KSHIELD_TOP
 local MT_PLAYER = MT_PLAYER
-local motype
+local abs = abs
+local searchBlockmap = searchBlockmap
+local S_StartSound = S_StartSound
+local pairs = pairs
 freeslot("sfx_graze")
 local itemtable = {
 	[MT_SSMINE_SHIELD] = true,
@@ -23,28 +26,30 @@ local itemtable = {
 	[MT_DROPTARGET] = true,
 	[MT_DROPTARGET_SHIELD] = true,
 	[MT_SPBEXPLOSION] = true,
+	[MT_PLAYER] = true,
 }
 
 local function isplayerhazardous(p)
-	return (p.invincibilitytimer or p.growshrinktimer > 0
-	or (p.flamedash and p.itemtype == KITEM_FLAMESHIELD)
+	return (p.invincibilitytimer or (p.growshrinktimer and p.growshrinktimer > 0)
+	or (p.flamedash and p.itemtype and p.itemtype == KITEM_FLAMESHIELD)
 	or p.bubbleblowup)
 end
 
 local function blockmapsearchfunc(pmo, mo)
-	motype = mo and mo.valid and mo.type
-	if pmo and pmo.valid and pmo.player and pmo.player.valid and motype 
-	and (itemtable[motype] or (motype == MT_PLAYER and mo.player and mo.player.valid and isplayerhazardous(mo.player)))
-	and abs(pmo.z - mo.z) <= pmo.height + mo.height	-- Pretty close in height too
-	and not ((mo.target and mo.target == pmo) or (mo == pmo))
+	if mo == pmo
+	or not (mo and mo.valid)
+	or mo.target == pmo
+	or (mo.player and mo.player.valid and not isplayerhazardous(mo.player)) then return end
+	if itemtable[mo.type]
+	and abs(pmo.z - mo.z) <= (pmo.height + mo.height)	-- Pretty close in height too
 		mo.grazetable = $ or {}
-		mo.grazetable[#pmo.player+1] = $ and $+1 or 1
-		if mo.grazetable[#pmo.player+1] == 1
+		mo.grazetable[#pmo.player] = $ and $+1 or 1
+		if mo.grazetable[#pmo.player] == 1
 			pmo.player.grazesthistic = $+TICRATE
-		elseif mo.grazetable[#pmo.player+1] < TICRATE
+		elseif mo.grazetable[#pmo.player] < TICRATE
 			pmo.player.grazesthistic = $+1
 		else
-			mo.grazetable[#pmo.player+1] = TICRATE
+			mo.grazetable[#pmo.player] = TICRATE
 		end
 	end
 end
@@ -56,7 +61,7 @@ addHook("MobjThinker", function(mo)
 	and not isplayerhazardous(p)
 		if not (p.exiting or p.spectator or p.flashing or p.spinouttimer or p.hyudorotimer or p.curshield == KSHIELD_TOP)
 			p.grazesthistic = 0
-			searchBlockmap("objects", blockmapsearchfunc, mo, mo.x - 99*mo.radius/70, mo.x + 99*mo.radius/70, mo.y - 99*mo.radius/70, mo.y + 99*mo.radius/70)
+			searchBlockmap("objects", blockmapsearchfunc, mo)
 			if p.grazesthistic
 				S_StartSound(nil, sfx_graze, p)
 				p.driftboost = $+p.grazesthistic
@@ -64,10 +69,7 @@ addHook("MobjThinker", function(mo)
 				--CONS_Printf(p, "grazing")
 			end
 		end
-		mo.grazetable = $ or {}
-		for k, v in ipairs(mo.grazetable)
-			v = $ and $-1 or 0
-		end
+		mo.grazetable = {}
 	end
 end, MT_PLAYER)
 
