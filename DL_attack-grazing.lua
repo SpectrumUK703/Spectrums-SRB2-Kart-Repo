3fe98ac5cd9c2a3f7a1b0933e4a76f50ac53eb2e
SpectrumUK703 <34438887+SpectrumUK703@users.noreply.github.com>
2024-06-14 03:59:43 +0100

    I think I'm actually done now

--- DL_attack-grazing.lua
+++ DL_attack-grazing.lua
@@ -8,11 +8,19 @@ local searchBlockmap = searchBlockmap
 local S_StartSound = S_StartSound
 local pairs = pairs
 freeslot("sfx_graze")
+
+local itemgrazing = CV_RegisterVar({
+	name = "itemgrazing",
+	defaultvalue = "On",
+	possibleValue = CV_OnOff,
+	flags = CV_NETVAR,
+	description = "Allows players to graze on opponents' attacking items for a speed boost."
+})
+
 local itemtable = {
 	[MT_SSMINE_SHIELD] = true,
 	[MT_ORBINAUT] = true,
 	[MT_ORBINAUT_SHIELD] = true,
-	[MT_JAWZ] = true,
 	[MT_JAWZ_SHIELD] = true,
 	[MT_BANANA] = true,
 	[MT_BANANA_SHIELD] = true,
@@ -21,7 +29,6 @@ local itemtable = {
 	[MT_GARDENTOP] = true,
 	[MT_GACHABOM] = true,
 	[MT_INSTAWHIP] = true,
-	[MT_SPB] = true,
 	[MT_SUPER_FLICKY] = true,
 	[MT_DROPTARGET] = true,
 	[MT_DROPTARGET_SHIELD] = true,
@@ -37,46 +44,54 @@ end
 
 local function blockmapsearchfunc(pmo, mo)
 	if mo == pmo
-	or not (mo and mo.valid)
 	or mo.target == pmo
 	or (mo.player and mo.player.valid and not isplayerhazardous(mo.player)) then return end
 	if itemtable[mo.type]
+	and R_PointToDist2(pmo.x, pmo.y, mo.x, mo.y) <= (pmo.radius + mo.radius)
 	and abs(pmo.z - mo.z) <= (pmo.height + mo.height)	-- Pretty close in height too
-		mo.grazetable = $ or {}
-		mo.grazetable[#pmo.player] = $ and $+1 or 1
-		if mo.grazetable[#pmo.player] == 1
-			pmo.player.grazesthistic = $+TICRATE
-		elseif mo.grazetable[#pmo.player] < TICRATE
-			pmo.player.grazesthistic = $+1
+		pmo.player.grazedrecently = $ or 7
+		mo.grazedtable = $ or {}
+		if not mo.grazedtable[#pmo.player]
+			mo.grazedtable[#pmo.player] = true
+			pmo.player.driftboost = $+TICRATE
 		else
-			mo.grazetable[#pmo.player] = TICRATE
+			pmo.player.driftboost = $+1
 		end
 	end
 end
 
-addHook("MobjThinker", function(mo)
-	if not (mo and mo.valid and mo.health) or mo.hitlag or (mo.flags2 & MF2_ALREADYHIT) then return end
-	local p = mo.player
-	if p and p.valid 
-	and not isplayerhazardous(p)
-		if not (p.exiting or p.spectator or p.flashing or p.spinouttimer or p.hyudorotimer or p.curshield == KSHIELD_TOP)
-			p.grazesthistic = 0
-			searchBlockmap("objects", blockmapsearchfunc, mo)
-			if p.grazesthistic
-				S_StartSound(nil, sfx_graze, p)
-				p.driftboost = $+p.grazesthistic
-				p.grazesthistic = 0
-				--CONS_Printf(p, "grazing")
-			end
-		end
-		mo.grazetable = {}
+local mo
+local hostmod
+addHook("ThinkFrame", function()
+	if not hostmod and HM_Scoreboard_AddMod --check for hostmod every frame, see if scoreboard is on.
+		HM_Scoreboard_AddMod({disp = "Grazing", var = "itemgrazing"}) --hostmod manages displaying/hiding this for us.
+		hostmod = true
 	end
-end, MT_PLAYER)
+	if not itemgrazing.value then return end
+	if leveltime <= starttime then return end
+	-- This was originally a MT_PLAYER MobjThinker lol
+	for p in players.iterate
+		mo = p.mo
+		if not (mo and mo.valid) then continue end
+		if not mo.health or mo.hitlag or (mo.flags2 & MF2_ALREADYHIT) then continue end
+		if not isplayerhazardous(p)
+			if not (p.exiting or p.spectator or p.flashing or p.spinouttimer or p.hyudorotimer or p.curshield == KSHIELD_TOP)
+				searchBlockmap("objects", blockmapsearchfunc, mo)
+				p.grazedrecently = $ and $-1 or 0
+				if p.grazedrecently == 1
+					S_StartSound(nil, sfx_graze, p)
+					--CONS_Printf(p, "grazing")
+				end
+			end
+			mo.grazedtable = {}
+		end
+	end
+end)
 
 addHook("MapLoad", function()
 	for p in players.iterate
 		if p.mo and p.mo.valid
-			p.mo.grazetable = {}
+			p.mo.grazedtable = {}
 		end
 	end
 end)
