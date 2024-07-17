2037f3cb08d6d0d32eed46f0c180bbd62fdde3a2
SpectrumUK703 <34438887+SpectrumUK703@users.noreply.github.com>
2024-07-17 12:04:20 +0100

    more files

---
+++ DL_SuperstarxalienColors_r2.lua
@@ -0,0 +1,90 @@
+freeslot(
+"SKINCOLOR_CHESTNUT",
+"SKINCOLOR_BATTERY",
+"SKINCOLOR_HALLOWEEN",
+"SKINCOLOR_SUNSET",
+"SKINCOLOR_HACKER",
+"SKINCOLOR_WORLD",
+"SKINCOLOR_PLANT",
+"SKINCOLOR_NERF"
+)
+
+skincolors[SKINCOLOR_CHESTNUT] = {
+    name = "Chestnut",
+    ramp = {0,51,222,66,67,68,69,63,63,45,46,47,47,29,30,31
+},
+    invcolor = SKINCOLOR_PLATINUM,
+    invshade = 6,
+    chatcolor = V_GOLDMAP,
+    accessible = true
+}	
+
+skincolors[SKINCOLOR_BATTERY] = {
+    name = "Battery",
+    ramp = {80,81,83,72,73,75,92,21,23,24,25,26,27,28,29,31
+},
+    invcolor = SKINCOLOR_BLUE,
+    invshade = 5,
+    chatcolor = V_YELLOWMAP,
+    accessible = true
+}	
+
+skincolors[SKINCOLOR_HALLOWEEN] = {
+    name = "Halloween",
+    ramp = {0,83,64,52,53,55,69,27,28,28,28,29,29,30,30,31
+},
+    invcolor = SKINCOLOR_MIDNIGHT,
+    invshade = 5,
+    chatcolor = V_ORANGEMAP,
+    accessible = true
+}
+
+skincolors[SKINCOLOR_SUNSET] = {
+    name = "Sunset",
+    ramp = {72,72,74,52,53,54,213,184,184,184,185,185,186,186,187,187
+},
+    invcolor = SKINCOLOR_PLAGUE,
+    invshade = 5,
+    chatcolor = V_ORANGEMAP,
+    accessible = true
+}
+
+skincolors[SKINCOLOR_HACKER] = {
+    name = "Hacker",
+    ramp = {96,112,112,113,113,114,114,27,28,28,28,29,29,30,30,31
+},
+    invcolor = SKINCOLOR_MAGENTA,
+    invshade = 5,
+    chatcolor = V_GREENMAP,
+    accessible = true
+}
+
+skincolors[SKINCOLOR_WORLD] = {
+    name = "World",
+    ramp = {0,147,123,124,125,150,154,156,157,138,127,118,119,159,253,254
+},
+    invcolor = SKINCOLOR_GOLD,
+    invshade = 10,
+    chatcolor = V_BLUEMAP,
+    accessible = true
+}
+
+skincolors[SKINCOLOR_PLANT] = {
+    name = "Plant",
+    ramp = {72,72,188,97,112,113,113,114,114,115,105,106,107,107,238,239
+},
+    invcolor = SKINCOLOR_TANGERINE,
+    invshade = 8,
+    chatcolor = V_GREENMAP,
+    accessible = true
+}
+
+skincolors[SKINCOLOR_NERF] = {
+    name = "Nerf",
+    ramp = {72,72,74,52,53,54,196,154,155,156,157,158,159,253,254,254
+},
+    invcolor = SKINCOLOR_PLANT,
+    invshade = 8,
+    chatcolor = V_BLUEMAP,
+    accessible = true
+}
