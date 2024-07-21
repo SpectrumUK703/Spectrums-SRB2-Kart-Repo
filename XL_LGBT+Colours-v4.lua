4f81be9adf1b30722098147548ef2f4a460e5e8f
SpectrumUK703 <34438887+SpectrumUK703@users.noreply.github.com>
2024-07-21 03:55:29 +0100

    Create XL_LGBT+Colours-v4.lua

---
+++ XL_LGBT+Colours-v4.lua
@@ -0,0 +1,131 @@
+freeslot(
+"SKINCOLOR_RAINBOW",
+"SKINCOLOR_LESBIAN",
+"SKINCOLOR_GAY",
+"SKINCOLOR_BISEXUAL",
+"SKINCOLOR_TRANSGENDER",
+"SKINCOLOR_NONBINARY",
+"SKINCOLOR_PANSEXUAL",
+"SKINCOLOR_ASEXUAL",
+"SKINCOLOR_AROMANTIC",
+"SKINCOLOR_GENDERQUEER",
+"SKINCOLOR_AROACE",
+"SKINCOLOR_POLYAM",
+"SKINCOLOR_POLYAM2"
+)
+
+skincolors[SKINCOLOR_RAINBOW] = {
+	name = "Rainbow",
+	ramp = {000,208,209,049,049,084,189,100,124,124,135,151,154,167,169,031},
+	invcolor = SKINCOLOR_PERIDOT,
+	invshade = 7,
+	chatcolor = V_PERIDOTMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_GAY] = {
+	name = "Gay",
+	ramp = {001,120,121,121,122,123,141,142,142,136,136,137,138,139,253,254},
+	invcolor = SKINCOLOR_PURPLE,
+	invshade = 4,
+	chatcolor = V_AQUAMAP,
+	accessible = true
+}
+
+skincolors[SKINCOLOR_LESBIAN] = {
+	name = "Lesbian",
+	ramp = {208,049,050,052,053,054,055,034,035,204,205,206,207,044,071,047},
+	invcolor = SKINCOLOR_PERIDOT,
+	invshade = 0,
+	chatcolor = V_ORANGEMAP,
+	accessible = true
+}
+
+skincolors[SKINCOLOR_BISEXUAL] = {
+	name = "Bisexual",
+	ramp = {000,252,176,178,179,180,163,164,165,166,153,154,156,157,159,254},
+	invcolor = SKINCOLOR_MINT,
+	invshade = 4,
+	chatcolor = V_PURPLEMAP,
+	accessible = true
+}
+
+skincolors[SKINCOLOR_TRANSGENDER] = {
+	name = "Transgender",
+	ramp = {000,001,128,129,130,131,131,161,161,202,203,203,204,205,207,187},
+	invcolor = SKINCOLOR_EMERALD,
+	invshade = 4,
+	chatcolor = V_SKYMAP,
+	accessible = true
+}
+
+skincolors[SKINCOLOR_ASEXUAL] = {
+	name = "Asexual",
+	ramp = {000,001,001,002,208,200,192,182,184,185,186,198,199,027,029,031},
+	invcolor = SKINCOLOR_AROMANTIC,
+	invshade = 7,
+	chatcolor = V_PURPLEMAP,
+	accessible = true
+}
+
+skincolors[SKINCOLOR_AROMANTIC] = {
+	name = "Aromantic",
+	ramp = {000,001,001,002,088,088,089,099,102,104,107,118,119,027,029,031},
+	invcolor = SKINCOLOR_ASEXUAL,
+	invshade = 7,
+	chatcolor = V_GREENMAP,
+	accessible = true
+}
+
+skincolors[SKINCOLOR_GENDERQUEER] = {
+	name = "Genderqueer",
+	ramp = {000,252,208,201,192,193,194,195,126,105,107,108,109,110,111,030},
+	invcolor = SKINCOLOR_YELLOW,
+	invshade = 5,
+	chatcolor = V_PURPLEMAP,
+	accessible = true
+}
+
+skincolors[SKINCOLOR_NONBINARY] = {
+	name = "Nonbinary",
+	ramp = {000,001,081,082,072,074,066,212,212,194,195,196,197,198,199,030},
+	invcolor = SKINCOLOR_TEAL,
+	invshade = 10,
+	chatcolor = V_YELLOWMAP,
+	accessible = true
+}
+
+skincolors[SKINCOLOR_PANSEXUAL] = {
+	name = "Pansexual",
+	ramp = {080,083,072,049,201,202,202,192,162,135,135,136,173,175,139,029},
+	invcolor = SKINCOLOR_TURQUOISE,
+	invshade = 4,
+	chatcolor = V_ROSYMAP,
+	accessible = true
+}
+
+skincolors[SKINCOLOR_AROACE] = {
+	name = "AroAce",
+	ramp = {000,080,081,082,050,051,221,091,141,135,136,136,137,138,139,139},
+	invcolor = SKINCOLOR_PURPLE,
+	invshade = 8,
+	chatcolor = V_ORANGEMAP,
+	accessible = true
+}
+
+skincolors[SKINCOLOR_POLYAM] = {
+	name = "Polyamorous",
+	ramp = {144,144,145,146,146,162,038,039,040,040,042,044,045,046,047,047},
+	invcolor = SKINCOLOR_GOLD,
+	invshade = 4,
+	chatcolor = V_REDMAP,
+	accessible = true
+}
+
+skincolors[SKINCOLOR_POLYAM2] = {
+	name = "PolyamProud",
+	ramp = {080,088,130,131,179,033,034,035,036,038,039,184,185,186,187,187},
+	invcolor = SKINCOLOR_PERIDOT,
+	invshade = 1,
+	chatcolor = V_REDMAP,
+	accessible = true
+}
