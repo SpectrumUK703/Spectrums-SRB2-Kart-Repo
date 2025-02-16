b4e28fd1808acc2ff522e65b02ddc1b31642713a
SpectrumUK703 <34438887+SpectrumUK703@users.noreply.github.com>
2025-02-16 17:51:20 +0000

    New mods (and many old files removed)

---
+++ DL_MSPATrollsHemoColors_v1.1.lua
@@ -0,0 +1,135 @@
+freeslot("SKINCOLOR_TEREZI"
+)
+
+skincolors[SKINCOLOR_TEREZI] = {
+	name = "Terezi",
+	ramp = {140,140,141,141,142,142,143,134,21,25,27,25,25,25,31,31
+	},
+	invcolor = SKINCOLOR_RUST,
+	invshade = 4, 
+	chatcolor = V_AQUAMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_VRISKA"
+)
+
+skincolors[SKINCOLOR_VRISKA] = {
+	name = "Vriska",
+	ramp = {133,133,134,136,137,137,137,138,21,25,27,25,25,25,31,31
+	},
+	invcolor = SKINCOLOR_COPPER,
+	invshade = 5, 
+	chatcolor = V_SKYMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_GAMZEE"
+)
+
+skincolors[SKINCOLOR_GAMZEE] = {
+	name = "Gamzee",
+	ramp = {164,165,165,166,166,167,168,168,21,25,27,25,28,31,31,31
+	},
+	invcolor = SKINCOLOR_COPPER,
+	invshade = 7, 
+	chatcolor = V_PURPLEMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_EQUIUS"
+)
+
+skincolors[SKINCOLOR_EQUIUS] = {
+	name = "Equius",
+	ramp = {151,152,154,154,156,156,157,158,21,25,27,28,28,31,31,31
+	},
+	invcolor = SKINCOLOR_TANGERINE,
+	invshade = 5, 
+	chatcolor = V_BLUEMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_NEPETA"
+)
+
+skincolors[SKINCOLOR_NEPETA] = {
+	name = "Nepeta",
+	ramp = {80,81,73,76,77,105,106,106,107,108,109,110,111,151,156,253
+	},
+	invcolor = SKINCOLOR_PURPLE,
+	invshade = 3, 
+	chatcolor = V_GREENMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_FEFERI"
+)
+
+skincolors[SKINCOLOR_FEFERI] = {
+	name = "Feferi",
+	ramp = {200,201,203,204,204,204,184,185,21,25,27,25,25,25,31,31
+	},
+	invcolor = SKINCOLOR_TEAL,
+	invshade = 4, 
+	chatcolor = V_MAGENTAMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_KANAYA"
+)
+
+skincolors[SKINCOLOR_KANAYA] = {
+	name = "Kanaya",
+	ramp = {96,96,100,101,101,102,105,107,21,25,27,25,25,25,31,31
+	},
+	invcolor = SKINCOLOR_CRIMSON,
+	invshade = 4, 
+	chatcolor = V_GREENMAP
+	,
+	accessible = true
+}	
+freeslot("SKINCOLOR_TAVROS"
+)
+	
+	skincolors[SKINCOLOR_TAVROS] = {
+	name = "Tavros",
+	ramp = {56,57,57,58,59,60,61,21,21,25,27,25,25,25,31,31
+	},
+	invcolor = SKINCOLOR_CERULEAN,
+	invshade = 5, 
+	chatcolor = V_ORANGEMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_KARKAT"
+)
+	
+	skincolors[SKINCOLOR_KARKAT] = {
+	name = "Karkat",
+	ramp = {17,18,19,20,21,22,22,24,24,24,25,26,31,31,31,31
+	},
+	invcolor = SKINCOLOR_RUST,
+	invshade = 4, 
+	chatcolor = V_GRAYMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_ARADIA"
+)
+
+	skincolors[SKINCOLOR_ARADIA] = {
+	name = "Aradia",
+	ramp = {42,44,45,45,46,46,20,21,21,22,25,29,29,30,31,31
+	},
+	invcolor = SKINCOLOR_CERULEAN,
+	invshade = 4, 
+	chatcolor = V_REDMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_SOLLUX"
+)
+
+	skincolors[SKINCOLOR_SOLLUX] = {
+	name = "Sollux",
+	ramp = {74,74,75,75,76,76,77,78,22,22,23,24,28,29,31,31
+	},
+	invcolor = SKINCOLOR_PURPLE,
+	invshade = 3, 
+	chatcolor = V_YELLOWMAP,
+	accessible = true
+
+}	
+		
