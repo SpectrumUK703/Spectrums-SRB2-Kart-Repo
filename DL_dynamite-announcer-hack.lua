6014056d92974a20e62ecf2871b20c12c0bee702
SpectrumUK703 <34438887+SpectrumUK703@users.noreply.github.com>
2025-04-04 21:56:34 +0100

    mod update

---
+++ DL_dynamite-announcer-hack.lua
@@ -0,0 +1,104 @@
+local after_announcer = dusa_an_enable
+local after_dynamite = bdd
+local cv_numlaps = CV_FindVar("numlaps")
+local cv_cheats = CV_FindVar("cheats")
+local resetlapsnexttic
+if (after_announcer and after_dynamite) or not (after_announcer or after_dynamite) then
+	print("This has to be added between Daytona Announcer and Bean's Dynamite Derby!")
+	return
+end
+
+--This is so fucking hacky but I want these mods to work together
+addHook("ThinkFrame", function()
+	cv_numlaps = $ or CV_FindVar("numlaps")
+	cv_cheats = $ or CV_FindVar("cheats")
+	if not (dusa_an_enable and bdd) then return end
+	if (gametyperules & GTR_CIRCUIT) then
+		if after_announcer then
+			rawset(_G, "gametype", nil)
+			if gametype == GT_BDD and mapheaderinfo[gamemap].typeoflevel != 0 then
+				if resetlapsnexttic then
+					if bdd.gameover
+						print("Lucky timing for 1st place! (I'm not fixing this lol)")
+					end
+					CV_StealthSet(cv_cheats, 1)
+					CV_Set(cv_numlaps, resetlapsnexttic)
+					CV_StealthSet(cv_cheats, 0)
+					resetlapsnexttic = nil
+				end
+				if bdd.gameover
+					for p in players.iterate
+						if p.spectator then continue end
+						if p.position == 1 and numlaps ~= p.laps-1
+							CV_StealthSet(cv_cheats, 1)
+							CV_Set(cv_numlaps, p.laps-1)
+							CV_StealthSet(cv_cheats, 0)
+						end
+					end
+				elseif bdd.numplayersremaining == 2 and (bdd.deathmessagetimer == 5 * TICRATE-1 or leveltime == 35)
+					for p in players.iterate
+						if p.spectator then continue end
+						if p.position == 1 and numlaps ~= p.laps
+							-- Yes, this does mean if 1st place is about to cross the finish line, they just win lol
+							resetlapsnexttic = numlaps
+							CV_StealthSet(cv_cheats, 1)
+							CV_Set(cv_numlaps, p.laps)
+							CV_StealthSet(cv_cheats, 0)
+						end
+					end
+				end
+			end
+		else
+			rawset(_G, "gametype", 0)
+		end
+	end
+end)
+
+addHook("PostThinkFrame", function()
+	rawset(_G, "gametype", nil)
+end)
+
+addHook("PreThinkFrame", function()
+	cv_numlaps = $ or CV_FindVar("numlaps")
+	cv_cheats = $ or CV_FindVar("cheats")
+	if not (dusa_an_enable and bdd) then return end
+	if (gametyperules & GTR_CIRCUIT) then
+		if after_announcer then
+			rawset(_G, "gametype", 0)
+		else
+			rawset(_G, "gametype", nil)
+			if gametype == GT_BDD and mapheaderinfo[gamemap].typeoflevel != 0 then
+				if resetlapsnexttic then
+					if bdd.gameover
+						print("Lucky timing for 1st place! (I'm not fixing this lol)")
+					end
+					CV_StealthSet(cv_cheats, 1)
+					CV_Set(cv_numlaps, resetlapsnexttic)
+					CV_StealthSet(cv_cheats, 0)
+					resetlapsnexttic = nil
+				end
+				if bdd.gameover
+					for p in players.iterate
+						if p.spectator then continue end
+						if p.position == 1 and numlaps ~= p.laps-1
+							CV_StealthSet(cv_cheats, 1)
+							CV_Set(cv_numlaps, p.laps-1)
+							CV_StealthSet(cv_cheats, 0)
+						end
+					end
+				elseif bdd.numplayersremaining == 2 and (bdd.deathmessagetimer == 5 * TICRATE-1 or leveltime == 35)
+					for p in players.iterate
+						if p.spectator then continue end
+						if p.position == 1 and numlaps ~= p.laps
+							-- Yes, this does mean if 1st place is about to cross the finish line, they just win lol
+							resetlapsnexttic = numlaps
+							CV_StealthSet(cv_cheats, 1)
+							CV_Set(cv_numlaps, p.laps)
+							CV_StealthSet(cv_cheats, 0)
+						end
+					end
+				end
+			end
+		end
+	end
+end)
